c53adbd26708fa618fcf38390cba5f0f8b09cf13
EdwarlyGamer999+ <212457124+edwarly999plus@users.noreply.github.com>
2026-05-30 18:33:30 -0400

    Add Spanish locale to LocaleList

--- src/locale/locale_list.ts
+++ src/locale/locale_list.ts
@@ -1,10 +1,13 @@
 import { localeEnglish } from "./locale_en/locale.ts";
+import { localeSpanish } from ".locale_es/locale.ts";
 import { localeFrench } from "./locale_fr/locale.ts";
 import { localePolish } from "./locale_pl/locale.ts";
+
 
 export const LocaleList: Record<string, { translation: object; name: string }> =
     {
         en: { translation: localeEnglish, name: "English" },
+        es: { translation: localeSpanish, name: "Spanish" },
         fr: { translation: localeFrench, name: "Français" },
         pl: { translation: localePolish, name: "Polski" }
     };
