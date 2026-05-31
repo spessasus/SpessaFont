66d97d521b62268b5d0d95e7c8f43c40d621c3b1
Stanley <spesekspesek@gmail.com>
2026-05-31 20:56:40 +0200

    Update src/locale/locale_list.ts

--- src/locale/locale_list.ts
+++ src/locale/locale_list.ts
@@ -1,5 +1,5 @@
 import { localeEnglish } from "./locale_en/locale.ts";
-import { localeSpanish } from ".locale_es/locale.ts";
+import { localeSpanish } from "./locale_es/locale.ts";
 import { localeFrench } from "./locale_fr/locale.ts";
 import { localePolish } from "./locale_pl/locale.ts";
 
